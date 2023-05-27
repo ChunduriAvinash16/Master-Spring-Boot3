41b297cadcd53e091d59229347a091acd2900a53
Avinash <chunduriavinash6@gmail.com>
2023-05-27 17:35:26 +0530

    Spring Security Changes for login

--- myfirstwebapp/src/main/java/com/learn/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/learn/springboot/myfirstwebapp/login/LoginController.java
@@ -13,27 +13,10 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @SessionAttributes("name")
 public class LoginController {
 
-    private AuthenticationService authenticationService;
-
-    public LoginController(AuthenticationService authenticationService) {
-        super();
-        this.authenticationService = authenticationService;
-    }
-
     private Logger log = LoggerFactory.getLogger(getClass());
-    @RequestMapping(value = "login", method = RequestMethod.GET)
-    public String gotoLoginPage() {
-        return "login";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String gotoWlcomePage() {
+        return "welcome";
     }
 
-    @RequestMapping(value = "login", method = RequestMethod.POST)
-    public String gotoWelcomeage(@RequestParam String name,@RequestParam String password,ModelMap model) {
-        if(authenticationService.authenticate(name,password)) {
-            model.put("name", name);
-            return "welcome";
-        }
-
-        model.put("error","Invalid Credentials ! Try Again!!!");
-        return  "login";
-    }
 }
